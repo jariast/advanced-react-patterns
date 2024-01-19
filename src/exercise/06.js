37c0d2ada6ac64e0e9f01764acba6fde0e1bde32
Juan Arias <jcamiloat@gmail.com>
2024-01-19 09:04:30 -0500

    feat: Ex06 Extra01 after watching solution

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -34,9 +34,7 @@ function useToggle({
   reducer = toggleReducer,
   onChange,
   on: controlledOn,
-  // 🐨 add an `onChange` prop.
-  // 🐨 add an `on` option here
-  // 💰 you can alias it to `controlledOn` to avoid "variable shadowing."
+  readOnly = false,
 } = {}) {
   const {current: initialState} = React.useRef({on: initialOn})
   const [state, dispatch] = React.useReducer(reducer, initialState)
@@ -45,24 +43,30 @@ function useToggle({
   // const shouldNotWarnReadOnly = controlledOn && !!onChange
 
   const onIsControlled = controlledOn != null
-  // 🐨 determine whether on is controlled and assign that to `onIsControlled`
-  // 💰 `controlledOn != null`
-
-  // 🐨 Replace the next line with `const on = ...` which should be `controlledOn` if
-  // `onIsControlled`, otherwise, it should be `state.on`.
-  // const {on} = state
   const on = onIsControlled ? controlledOn : state.on
+  const hasOnChangeFunction = !!onChange
 
-  //Extra 01
-  if (onIsControlled) {
-    const shouldNotWarnReadOnly = !!onChange
+  //Extra 01 Before Watching solution
+  // if (onIsControlled) {
+  //   const shouldNotWarnReadOnly = !!onChange
+  //
+  //   warning(
+  //     shouldNotWarnReadOnly,
+  //     'Failed prop type: You provided a `on` prop without an `onChange handler` ',
+  //   )
+  // }
+  // const shouldNotWarnReadOnly = onIsControlled || !!onChange
+
+  // Extra 01 After watching solution
+  React.useEffect(() => {
+    const shouldWarnReadOnly =
+      onIsControlled && !hasOnChangeFunction && !readOnly
 
     warning(
-      shouldNotWarnReadOnly,
-      'Failed prop type: You provided a `on` prop without an `onChange handler` ',
+      !shouldWarnReadOnly,
+      'Passed on without an onChange function, set readOnly to avoid this warning',
     )
-  }
-  // const shouldNotWarnReadOnly = onIsControlled || !!onChange
+  }, [onIsControlled, hasOnChangeFunction, readOnly])
 
   // We want to call `onChange` any time we need to make a state change, but we
   // only want to call `dispatch` if `!onIsControlled` (otherwise we could get
@@ -82,24 +86,6 @@ function useToggle({
     }
   }
 
-  // 🦉 "Suggested changes" refers to: the changes we would make if we were
-  // managing the state ourselves. This is similar to how a controlled <input />
-  // `onChange` callback works. When your handler is called, you get an event
-  // which has information about the value input that _would_ be set to if that
-  // state were managed internally.
-  // So how do we determine our suggested changes? What code do we have to
-  // calculate the changes based on the `action` we have here? That's right!
-  // The reducer! So if we pass it the current state and the action, then it
-  // should return these "suggested changes!"
-  //
-  // 💰 Sorry if Olivia the Owl is cryptic. Here's what you need to do for that onChange call:
-  // `onChange(reducer({...state, on}, action), action)`
-  // 💰 Also note that user's don't *have* to pass an `onChange` prop (it's not required)
-  // so keep that in mind when you call it! How could you avoid calling it if it's not passed?
-
-  // make these call `dispatchWithOnChange` instead
-  // const toggle = () => dispatch({type: actionTypes.toggle})
-  // const reset = () => dispatch({type: actionTypes.reset, initialState})
   const toggle = () => dispatchWithOnChange({type: actionTypes.toggle})
   const reset = () =>
     dispatchWithOnChange({type: actionTypes.reset, initialState})
@@ -128,12 +114,13 @@ function useToggle({
   }
 }
 
-function Toggle({on: controlledOn, onChange, initialOn, reducer}) {
+function Toggle({on: controlledOn, onChange, initialOn, reducer, readOnly}) {
   const {on, getTogglerProps} = useToggle({
     on: controlledOn,
     onChange,
     initialOn,
     reducer,
+    readOnly,
   })
   const props = getTogglerProps({on})
   return <Switch {...props} />
@@ -161,6 +148,7 @@ function App() {
       <div>
         <Toggle on={bothOn} onChange={handleToggleChange} />
         {/*Extra 01*/}
+        <Toggle on={bothOn} readOnly={true} />
         <Toggle on={bothOn} />
         <Toggle on={bothOn} onChange={handleToggleChange} />
       </div>
